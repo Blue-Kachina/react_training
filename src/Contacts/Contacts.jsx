dca651d9b42527b0a58875e637bbef59e8a39e39
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-05-09 12:00:06 -0400

    Added contacts from API into contacts list

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -1,5 +1,33 @@
+import ContactAPI from './ContactApiAzure'
+import {use} from "react";
+import {Table} from 'react-bootstrap';
+
 export default function Contacts() {
+
+    const contacts = use(ContactAPI.getAllContacts())
+
     return <>
         <h1>Contacts</h1>
+        <Table striped hover>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {contacts.map((contact) =>
+                    <tr key={contact.id}>
+                        <td>{contact.id}</td>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                    </tr>
+                )}
+            </tbody>
+        </Table>
     </>
 }
